5bfc026a4a091f50a6ec49a9adc7d79d42c9495a
Ivan Lashuk <85491523+Britva1910@users.noreply.github.com>
2022-03-28 21:43:31 +0300

    feat: add solution count-cats and carbon-dating

--- src/carbon-dating.js
+++ src/carbon-dating.js
@@ -17,11 +17,20 @@ const HALF_LIFE_PERIOD = 5730;
  * dateSample('WOOT!') => false
  *
  */
-function dateSample(/* sampleActivity */) {
-  throw new NotImplementedError('Not implemented');
-  // remove line with error and write your code here
+function dateSample(sampleActivity) {
+    let num = Number(sampleActivity);
+    if (typeof sampleActivity != 'string') {
+        return false;
+    } else if (num <= 0 || num > MODERN_ACTIVITY) {
+        return false;
+    } else if (isNaN(num) || num == 0) {
+        return false;
+    } else {
+        let time = Math.log(MODERN_ACTIVITY / sampleActivity) / (0.693 / HALF_LIFE_PERIOD);
+        return Math.ceil(time);
+    }
 }
 
 module.exports = {
-  dateSample
+    dateSample
 };
